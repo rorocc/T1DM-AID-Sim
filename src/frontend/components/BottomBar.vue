73290fa944183f7fa2d818aa97307b9dfc647b5b
crock <christopher.rock@student.uni-luebeck.de>
2022-07-08 21:20:57 +0200

    bottom bar

--- src/frontend/components/BottomBar.vue
+++ src/frontend/components/BottomBar.vue
@@ -1,3 +1,22 @@
 <template>
-  BottomBar
+  <div class="tab-container absolute bottom-0 w-full bg-white">
+    <div class="flex justify-between">
+      <div class="tab-btn">Szenarien</div>
+      <div class="tab-btn">Vergleichen</div>
+      <div class="tab-btn">Simulieren</div>
+    </div>
+  </div>
+
 </template>
+
+<style>
+
+.tab-container{
+  filter: drop-shadow(0px 23px 45px rgba(0, 0, 0, 0.25));
+}
+
+.tab-btn{
+  @apply px-6 py-4;
+}
+
+</style>
