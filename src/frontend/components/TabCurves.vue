0add8360d2788ddd5e4298f9d8a48d9aaacc8f5b
emkes <emkes@imis.uni-luebeck.de>
2022-07-12 11:13:42 +0200

    working in second curve (doesnt show anything yet)

--- src/frontend/components/TabCurves.vue
+++ src/frontend/components/TabCurves.vue
@@ -2,16 +2,19 @@
   <ContentContainer>
     <h1>Kurven</h1>
     <ChartCurve></ChartCurve>
+    <h1>Basalrate + Bolus</h1>
+    <ChartInsulinCurve></ChartInsulinCurve>
   </ContentContainer>
 </template>
 
 <script>
 import ContentContainer from "./ContentContainer.vue";
 import ChartCurve from "./ChartCurve.vue";
+import ChartInsulinCurve from "./ChartInsulinCurve.vue"
 
 export default {
   name: "TabCurves",
-  components: {ContentContainer, ChartCurve}
+  components: {ContentContainer, ChartCurve, ChartInsulinCurve}
 }
 </script>
 
